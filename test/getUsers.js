db43537f5bbe1eb717d4fa38779e3e25d347b32f
gaboxxz <gaboxxz@gmail.com>
2019-09-23 17:51:02 -0300

    Added factory-girl use

--- test/getUsers.js
+++ test/getUsers.js
@@ -1,50 +1,65 @@
 const app = require('../server');
+const { user } = require('../app/models/');
+const helpers = require('../app/helpers');
 const supertest = require('supertest');
+const { factory } = require('factory-girl');
 const validUser = {
-  first_name: 'TestName',
-  last_name: 'TestLastName',
+  firstName: 'TestName',
+  lastName: 'TestLastName',
   password: '12345678Ab',
   email: 'Test@wolox.com'
 };
 const validSignIn = { email: 'Test@wolox.com', password: '12345678Ab' };
 
-const request = supertest(app);
-describe.only('Get /users', () => {
-  let response = null;
-  beforeEach(done => {
-    request
-      .post('/users')
-      .set('Content-Type', 'application/json')
-      .set('Acccept', 'application/json')
-      .send(validUser)
-      .then(() => {
-        request
-          .post('/users/sessions')
-          .set('Content-Type', 'application/json')
-          .set('Acccept', 'application/json')
-          .send(validSignIn)
-          .end((err, res) => {
-            if (err) {
-              throw err;
-            }
-            response = res;
-            done();
-          });
-      });
-  });
+factory.define(
+  'user',
+  user,
+  {
+    firstName: factory.chance('name', { middle: true }),
+    lastName: factory.chance('name', { middle: true }),
+    email: factory.chance('email', { domain: 'wolox.com' }),
+    password: factory.chance('string', { length: 8 })
+  },
+  {
+    afterBuild: model => {
+      model.password = helpers.hashPassword(model.password);
+      return model;
+    }
+  }
+);
 
-  it('Get users list', () =>
-    request
-      .get('/users')
-      .set('Content-Type', 'application/json')
-      .set('Acccept', 'application/json')
-      .set('authorization', response.body.session.token)
-      .query({ pageSize: '1', page: '0' })
-      .send()
-      .then(res => {
-        expect(res.body).toHaveProperty('count');
-        expect(res.body).toHaveProperty('rows');
-        expect(res.status).toBe(200);
-        expect(res.body.rows.length).toBe(1);
-      }));
+const request = supertest(app);
+
+describe('Get /users', () => {
+  let token = null;
+  beforeEach(() =>
+    Promise.all([factory.createMany('user', 15), factory.create('user', validUser)]).then(() =>
+      request
+        .post('/users/sessions')
+        .send(validSignIn)
+        .then(res => {
+          // eslint-disable-next-line prefer-destructuring
+          token = res.body.session.token;
+        })
+    )
+  );
+
+  it.each([[0, 1, 1], [1, 3, 3], [3, 5, 1]])(
+    'Page: %i pageSize: %i should return %i',
+    (page, pageSize, expected) =>
+      request
+        .get('/users')
+        .set('Content-Type', 'application/json')
+        .set('Acccept', 'application/json')
+        .set('authorization', token)
+        .query({ pageSize, page })
+        .send()
+        .then(res => {
+          expect(res.body).toHaveProperty('count');
+          // expect(res.body.count).toBe(expected);
+          expect(res.body).toHaveProperty('rows');
+          expect(res.status).toBe(200);
+          expect(res.body.rows.length).toBe(expected);
+        })
+  );
 });
