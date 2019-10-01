2911c3fe170ed3ff5a446aa8b3cb266600c93bd1
gaboxxz <gaboxxz@gmail.com>
2019-10-01 17:25:09 -0300

    Admin user support.

--- app/serializers/users.js
+++ app/serializers/users.js
@@ -4,7 +4,8 @@ exports.serializeCreatedUser = createdUser => {
       id: createdUser.dataValues.id,
       first_name: createdUser.dataValues.firstName,
       last_name: createdUser.dataValues.lastName,
-      email: createdUser.dataValues.email
+      email: createdUser.dataValues.email,
+      is_admin: createdUser.isAdmin
     }
   };
   return serializedUser;
