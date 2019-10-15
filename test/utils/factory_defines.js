bca289a3c8a13cd7ce5cbca32ef609d2a4722151
gaboxxz <gaboxxz@gmail.com>
2019-10-15 14:19:57 -0300

    Changed emal createion to avoid chance error

--- test/utils/factory_defines.js
+++ test/utils/factory_defines.js
@@ -1,14 +1,13 @@
 const { user, albumsByUser } = require('../../app/models/');
 const helpers = require('../../app/helpers');
 const { factory } = require('factory-girl');
-
 factory.define(
   'user',
   user,
   {
     firstName: factory.chance('name', { middle: true }),
     lastName: factory.chance('name', { middle: true }),
-    email: factory.chance('email', { domain: 'wolox.com' }),
+    email: factory.sequence('User.email', n => `dummy-user-${n}@wolox.com`),
     password: factory.chance('string', { length: 8 })
   },
   {
